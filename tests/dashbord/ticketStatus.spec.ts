ef153381ba67f358e2fe85dba821d6d5ae0a792e
TomerTTB <tomergranit@hotmail.com>
2024-05-09 22:52:04 +0300

    Update ticketStatus.spec.ts

--- tests/dashbord/ticketStatus.spec.ts
+++ tests/dashbord/ticketStatus.spec.ts
@@ -5,7 +5,7 @@ const testConfig = require('../../config/testConfig.json')
 test.describe('Assert open tickets', () => {
   test('sideBar --> Assert open tickets', async ({ page, loginPage, sidebarPage, ticketStatusPage }) => {
     test.slow();
-    //await loginPage.login();
+    await loginPage.login();
     await sidebarPage.navigateToMenuItem(testConfig);
     await ticketStatusPage.validateStatus();
   });
