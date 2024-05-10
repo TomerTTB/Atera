7eb647a68bde7dd6fce9c45d5a76b5af2aedb0ee
TomerTTB <tomergranit@hotmail.com>
2024-05-10 20:31:43 +0300

    add

--- tests/dashbord/ticketStatus.spec.ts
+++ tests/dashbord/ticketStatus.spec.ts
@@ -5,7 +5,7 @@ const testConfig = require('../../config/testConfig.json')
 test.describe('Assert open tickets', () => {
   test('sideBar --> Assert open tickets', async ({ loginPage, sidebarPage, ticketStatusPage, }) => {
     test.slow();
-    
+
     const fileStats = fs.statSync(<string>process.env.LOGINSTATEPATH);
     if (fileStats.size < 100) {
       await loginPage.login();
